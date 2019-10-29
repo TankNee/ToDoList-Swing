63714e4a84d5c71c2bc65cfbc198f891b7e2db25
tanknee <nitongke@gmail.com>
2019-10-29 18:52:04 +0800

    update readme and mainpart

--- src/MainPart.java
+++ src/MainPart.java
@@ -1,3 +1,34 @@
-public class MainPart {
+import javax.swing.*;
+
+public class MainPart extends JFrame {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public  void init() {
+        JPanel titlePanel = new JPanel();
+        JLabel titleLabel = new JLabel("Personal Agency");
+        titlePanel.add(titleLabel);
+        this.add(titlePanel);
+
+    }
+    public MainPart() {
+        super("Personal Agency");
+
+        this.setSize(360,720);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        init();
+        this.setResizable(false);
+        this.setVisible(true);
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        new MainPart();
+        
+    }
+   
     
 }
